77c787b3ab9ac70f8f4b6092e44310da9d4a6f2c
Koff <fsanmartinjorquera@uh.edu>
2015-12-20 20:50:19 -0800

    Footer fixed

--- reservations/forms.py
+++ reservations/forms.py
@@ -23,3 +23,13 @@ class LogInForm(forms.Form):
 
     class Meta:
         model = User
+
+
+class ProfileForm(forms.Form):
+    first_name = forms.CharField(max_length=30)
+    last_name = forms.CharField(max_length=30)
+    phone = forms.CharField(max_length=30)
+    email = forms.EmailField(max_length=254)
+
+    class Meta:
+        model = User
--- reservations/views.py
+++ reservations/views.py
@@ -3,19 +3,15 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views.generic import TemplateView, FormView, CreateView, ListView
 from .models import Reservation, User, Trip
-from .forms import RegistrationForm, LogInForm
+from .forms import RegistrationForm, LogInForm, ProfileForm
 from django.core.urlresolvers import reverse
 from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout
 from django.contrib import messages
-
-
-class LoginRequiredMixin(object):
-    @classmethod
-    def as_view(cls, **initkwargs):
-        view = super(LoginRequiredMixin, cls).as_view(**initkwargs)
-        return login_required(view)
+from django.utils import timezone
+from datetime import datetime
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 class ReserveView(TemplateView):
@@ -77,40 +73,63 @@ class LogInView(FormView):
 
 
 class HomeView(LoginRequiredMixin, ListView):
+    login_url = '/login/'
     model = Trip
-    queryset = Trip.objects.all().order_by('departure_date')[:5]
+    past_trips = Trip.objects.all().filter(departure_date__lt=timezone.now()).order_by('departure_date')[:5]
+    upcoming_trips = Trip.objects.all().filter(departure_date__gt=timezone.now()).order_by('departure_date')[:5]
     template_name = 'reservations/home.html'
 
     def get(self, request, *args, **kwargs):
-        return render(request, self.template_name, {'queryset': self.queryset})
+        return render(request, self.template_name, {'upcoming_trips': self.upcoming_trips,
+                                                    'past_trips': self.past_trips})
 
 
 class MyTripsView(LoginRequiredMixin, ListView):
+    login_url = '/login/'
     model = Trip
     template_name = 'reservations/my_trips.html'
 
     def get(self, request, *args, **kwargs):
-        queryset = []
+        upcoming_trips = []
+        past_trips = []
         reservations = Reservation.objects.all().filter(user_id_id=request.user.id)
         for reservation in reservations:
             trip = Trip.objects.all().filter(id=reservation.trip_id_id)[0]
-            seat = reservation.seat
-            queryset.append({'departure_city': trip.departure_city,
-                             'arrival_city': trip.arrival_city,
-                             'departure_date': trip.departure_date,
-                             'arrival_date': trip.arrival_date,
-                             'seat': seat})
-        return render(request, self.template_name, {'queryset': queryset})
+            if trip.arrival_date >= timezone.now():
+                seat = reservation.seat
+                upcoming_trips.append({'departure_city': trip.departure_city,
+                                       'arrival_city': trip.arrival_city,
+                                       'departure_date': trip.departure_date,
+                                       'arrival_date': trip.arrival_date,
+                                       'seat': seat})
+            elif trip.arrival_date < timezone.now():
+                seat = reservation.seat
+                past_trips.append({'departure_city': trip.departure_city,
+                                   'arrival_city': trip.arrival_city,
+                                   'departure_date': trip.departure_date,
+                                   'arrival_date': trip.arrival_date,
+                                   'seat': seat})
+        return render(request, self.template_name, {'upcoming_trips': upcoming_trips, 'past_trips': past_trips})
 
 
-class Profile(LoginRequiredMixin, ListView):
-    model = User
+class Profile(LoginRequiredMixin, FormView):
+    login_url = '/login/'
+    form_class = ProfileForm
     template_name = 'reservations/profile.html'
 
+    def form_valid(self, form):
+        user = self.request.user
+        user.first_name = form.data['first_name']
+        user.last_name = form.data['last_name']
+        user.save()
+        return render(self.request, 'reservations/profile.html', {'queryset': self.request.user})
+
+    def form_invalid(self, form):
+        messages.add_message(self.request, messages.ERROR, 'Something went wrong...')
+        return render(request=self.request, template_name=self.template_name)
+
     def get(self, request, *args, **kwargs):
-        queryset = []
-        print(request.user)
-        return render(request, self.template_name, {'queryset': queryset})
+        return render(request, self.template_name, {'queryset': request.user})
 
 
 class Logout(LoginRequiredMixin, ListView):
